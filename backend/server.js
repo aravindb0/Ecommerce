9bddf98bbb0d65eda5384f61f3c75dcfd10a32ee
Aravind <aravindbuddha1@gmail.com>
2026-05-16 23:24:33 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ import productRoutes from "./routes/product.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://mart-eight-sigma.vercel.app/",
+  Credentials:true
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
